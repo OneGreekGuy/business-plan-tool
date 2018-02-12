c2922a1238e4356bdc2db0eb15681a8afaea3bf6
OneGreekGuy <k_ostakis@hotmail.com>
2018-02-12 22:50:14 +0200

    Add files via upload

--- Frontend/3_human-resources/Εξωτερικοι Συνεργατες/logic.js
+++ Frontend/3_human-resources/Εξωτερικοι Συνεργατες/logic.js
@@ -11,8 +11,65 @@ var app = new Vue({
       save() {
         console.log('Οι εξωτερικοί συνεργάτες που αποθηκεύτηκαν είναι:')
         console.log(this.externalPartners)
+
+
+         for (var i=0 ; i < this.externalPartners.length; i++ ) {
+        
+      
+          axios.post('http://localhost:52800/api/Partner',{
+
+          ID: 1,
+          BusinessPlanId: 1,
+          Name: this.externalPartners[i].name,
+          SurName: this.externalPartners[i].surName,
+          Expertise: this.externalPartners[i].expertise,
+          LinkedIn: this.externalPartners[i].linkedIn,
+          From: this.externalPartners[i].from.replace('/','').replace('/',''),
+          Until: this.externalPartners[i].until.replace('/','').replace('/',''),
+          Duties: this.externalPartners[i].duties,
+
+          })
+           .then(function (response) {
+            console.log(response);
+          })
+           .catch(function (error) {
+            console.log(error);
+          });
+         }
+
+
+
+      },
+
+      showInstructions() {
+        // Get the modal
+
+      var modal = document.getElementById("myInstructions3");
+
+      // Get the button that opens the modal
+      var btn = document.getElementById("Instructions3");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks the button, open the modal 
+     
+          modal.style.display = "block";
+      
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          modal.style.display = "none";
       }
-    }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+    },
+  }
 })
 
 

c609bf96dd58565784f41833d8fbd48543d68cf5
OneGreekGuy <k_ostakis@hotmail.com>
2018-02-12 22:49:16 +0200

    Add files via upload

--- Frontend/3_human-resources/Ομαδα/logic.js
+++ Frontend/3_human-resources/Ομαδα/logic.js
@@ -1,19 +1,65 @@
 var app = new Vue({
-    el: 'main',
-    data: {
-      managers: [],
-      jobOptions: ['CEO', 'CTO', 'CFO']
+  el: 'main',
+  data: {
+    managers: [],
+    jobOptions: ['CEO', 'CTO', 'CFO']
+  },
+  methods: {
+    create() {
+      var newManager = { name: '', surName: '', job: '', linkedIn: '' }
+      this.managers.push(newManager)
     },
-    methods: {
-      create() {
-        var newManager = { name: '', surName: '', job: '', linkedIn: '' }
-        this.managers.push(newManager)
-      },
-      save() {
-          console.log('Οι managers που αποθηκεύτηκαν είναι:')
-          console.log(this.managers)
-      },
+    save() {
+      
+     for (var i=0 ; i < this.managers.length; i++ ) {
+     
+      
+     axios.post('http://localhost:52800/api/Manager',{
+         
+        ID: 1,
+        BusinessPlanId: 1,
+        Name: this.managers[i].name,
+        Surname: this.managers[i].surName,
+        Job: this.managers[i].job,
+        LinkedIn: this.managers[i].linkedIn,
+    
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
+
+      console.log('Οι managers που αποθηκεύτηκαν είναι:')
+      console.log(this.managers)
+    
+    },
+
+    showInstructions() {
+      var modal = document.getElementById("myInstructions1");
+
+      var btn = document.getElementById("Instructions1");
+
+      var span = document.getElementsByClassName("close")[0];
+
+
+
+      modal.style.display = "block";
+      
+
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    },
+  }
 })
 
 // "managers": [
